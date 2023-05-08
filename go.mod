1dafbe4338620f94114345c16a2ef599af080765
Yentl Frickx <frickx.yentl@gmail.com>
2023-05-08 22:35:37 +0200

    Finished first golang test

--- go.mod
+++ go.mod
@@ -3,16 +3,32 @@ module traefik-cloudflare-updater
 go 1.18
 
 require (
+	github.com/chyeh/pubip v0.0.0-20170203095919-b7e679cf541c
 	github.com/cloudflare/cloudflare-go v0.66.0
+	github.com/docker/docker v23.0.6+incompatible
 	github.com/sirupsen/logrus v1.9.0
 )
 
 require (
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	gotest.tools/v3 v3.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,29 +1,137 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/cloudflare/cloudflare-go"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/swarm"
 	log "github.com/sirupsen/logrus"
+	"net"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/docker/docker/api/types"
+	dockerClient "github.com/docker/docker/client"
+
+	"github.com/chyeh/pubip"
 )
+
+type CloudflareUpdater struct {
+	DockerClient  *dockerClient.Client
+	CloudflareApi *cloudflare.API
+	Ip            net.IP
+	Domains       []string
+}
+
+func boolPointer(b bool) *bool {
+	return &b
+}
+
+func (c *CloudflareUpdater) updateDomains() {
+	zoneIdentifier := cloudflare.ZoneIdentifier(os.Getenv("CF_ZONE_ID"))
+
+	for _, domain := range c.Domains {
+		records, _, err := c.CloudflareApi.ListDNSRecords(context.Background(), zoneIdentifier, cloudflare.ListDNSRecordsParams{Name: domain})
+		if err != nil {
+			log.Errorln(err)
+		} else {
+			if len(records) > 0 {
+				params := cloudflare.UpdateDNSRecordParams{
+					Type:    "A",
+					Name:    domain,
+					ID:      records[0].ID,
+					Content: c.Ip.String(),
+					TTL:     1,
+					Proxied: boolPointer(true),
+					Tags:    []string{"Created from traefik"},
+				}
+				_, err := c.CloudflareApi.UpdateDNSRecord(context.Background(), zoneIdentifier, params)
+				if err != nil {
+					log.Errorln(err)
+				}
+			} else {
+				params := cloudflare.CreateDNSRecordParams{
+					Type:    "A",
+					Name:    domain,
+					Content: c.Ip.String(),
+					TTL:     1,
+					Proxied: boolPointer(true),
+					Tags:    []string{"Created from traefik"},
+				}
+				_, err := c.CloudflareApi.CreateDNSRecord(context.Background(), zoneIdentifier, params)
+				if err != nil {
+					log.Errorln(err)
+				}
+			}
+		}
+	}
+}
+
+func (c *CloudflareUpdater) processService(service swarm.Service) {
+	for label, value := range service.Spec.Labels {
+		if strings.Contains(label, "rule") && strings.Contains(value, "Host") {
+			c.checkRule(value)
+		}
+	}
+}
+
+func (c *CloudflareUpdater) checkRule(value string) {
+	tld := os.Getenv("TLD")
+	if strings.Contains(value, tld) {
+		subdomain := strings.TrimSuffix(strings.TrimPrefix(value, "Host(`"), fmt.Sprintf(".%s`)", tld))
+		log.Infoln("Found domain: " + subdomain)
+		c.Domains = append(c.Domains, subdomain)
+	}
+}
+
+func (c *CloudflareUpdater) initialCheck() {
+	log.Infoln("Running initial check")
+	// List all running services in the Docker Swarm with the traefik.enable=true label
+	labelFilter := filters.NewArgs()
+	labelFilter.Add("label", "traefik.enable=true")
+
+	services, err := c.DockerClient.ServiceList(context.Background(), types.ServiceListOptions{Filters: labelFilter})
+	if err != nil {
+		log.Fatalf("failed to list Docker services: %v", err)
+	}
+
+	for _, service := range services {
+		c.processService(service)
+	}
+
+	c.updateDomains()
+}
 
 func main() {
 	api, err := cloudflare.NewWithAPIToken(os.Getenv("CF_TOKEN"))
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	ip, err := pubip.Get()
+	if err != nil {
+		fmt.Println("Couldn't get my IP address:", err)
+	}
+
+	// Docker client initialization
+	client, err := dockerClient.NewClientWithOpts(dockerClient.FromEnv, dockerClient.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatalf("failed to create Docker client: %v", err)
+	}
+
+	cloudflareUpdater := CloudflareUpdater{
+		DockerClient:  client,
+		CloudflareApi: api,
+		Ip:            ip,
+		Domains:       []string{},
+	}
+
+	cloudflareUpdater.initialCheck()
+
 	for {
-		status := getCurrentStatus()
-		log.Infoln(status)
-		if status != lastStatus {
-			log.Infoln("Sending alert")
-			success := sendPBAlert(status, pushBulletApiKey, deviceId)
-			if success {
-				lastStatus = status
-			}
-		}
+
 		time.Sleep(10 * time.Second)
 	}
 
